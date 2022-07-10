1b03a9a6c9b485744140981f524415fe482dcfc4
MJB <matt.berti13@gmail.com>
2022-07-10 17:08:12 -0500

    built and tested shopping cart reducers

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -32,11 +32,55 @@ export const reducer = (state, action) => {
       };
 
     case ADD_TO_CART:
-      return{
+      return {
         ...state,
         cartOpen: true,
         cart: [...state.cart, action.product]
       };
+
+    case ADD_MULTIPLE_TO_CART:
+      return {
+        ...state,
+        cart: [...state.cart, ...action.products]
+      };
+
+    case REMOVE_FROM_CART:
+      let newState = state.cart.filter(product => {
+        return product._id !== action._id;
+      });
+
+      return {
+        ...state,
+        cartOpen: newState.length > 0,
+        cart: newState
+      };
+
+    case UPDATE_CART_QUANTITY:
+      return{
+        ...state,
+        cartOpen: true,
+        // ? use map because original state should be immutable
+        cart: state.cart.map(product =>{
+          if(action._id === product._id){
+            product.purchaseQuantity = action.purchaseQuantity;
+          }
+          return product;
+        })
+      };
+    
+    case CLEAR_CART:
+      return{
+        ...state,
+        cartOpen: false,
+        cart: []
+      };
+
+    case TOGGLE_CART:
+      return{
+        ...state,
+        cartOpen: !state.cartOpen
+      };
+
 
     default:
       return state;
