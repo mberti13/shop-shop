b432de134715275220d67758d014959f6a4a91a6
MJB <matt.berti13@gmail.com>
2022-07-10 17:48:33 -0500

    users can add items to cart

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -4,12 +4,24 @@ import { useQuery } from '@apollo/client';
 import Cart from '../components/Cart';
 
 import { useStoreContext } from '../utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../utils/actions';
 import { QUERY_PRODUCTS } from '../utils/queries';
 import spinner from '../assets/spinner.gif';
+import{
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  ADD_TO_CART,
+  UPDATE_PRODUCTS,
+} from '../utils/actions';
 
 function Detail() {
   const [state, dispatch] = useStoreContext();
+
+  const addToCart = () =>{
+    dispatch({
+      type:ADD_TO_CART,
+      product: { ...currentProduct, purchaseQuantity: 1 }
+    });
+  };
   const { id } = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({});
@@ -41,7 +53,7 @@ function Detail() {
 
           <p>
             <strong>Price:</strong>${currentProduct.price}{' '}
-            <button>Add to Cart</button>
+            <button onClick={addToCart}>Add to Cart</button>
             <button>Remove from Cart</button>
           </p>
 
