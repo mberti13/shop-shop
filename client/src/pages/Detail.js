6f3b1e957ed0a93b69d54596770b0dc61b3d5fd8
MJB <matt.berti13@gmail.com>
2022-07-10 18:00:49 -0500

    shopping cart is functional

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -16,19 +16,37 @@ import{
 function Detail() {
   const [state, dispatch] = useStoreContext();
 
-  const addToCart = () =>{
+  const addToCart = () => {
+    const itemInCart = cart.find((cartItem) => cartItem._id === id);
+  
+    if (itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: { ...currentProduct, purchaseQuantity: 1 }
+      });
+    }
+  };
+
+  const removeFromCart = () =>{
     dispatch({
-      type:ADD_TO_CART,
-      product: { ...currentProduct, purchaseQuantity: 1 }
+      type: REMOVE_FROM_CART,
+      _id: currentProduct._id
     });
   };
+
   const { id } = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({});
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const { products } = state;
+  const { products, cart } = state;
 
   useEffect(() => {
     if (products.length) {
@@ -54,7 +72,12 @@ function Detail() {
           <p>
             <strong>Price:</strong>${currentProduct.price}{' '}
             <button onClick={addToCart}>Add to Cart</button>
-            <button>Remove from Cart</button>
+            <button
+            disabled={!cart.find(p => p._id === currentProduct._id)}
+            onClick={removeFromCart}
+            >
+              Remove from Cart
+              </button>
           </p>
 
           <img
