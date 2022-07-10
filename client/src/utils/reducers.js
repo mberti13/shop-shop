d0e60cbefb201bded184eb7de2a59a4c3476723a
MJB <matt.berti13@gmail.com>
2022-07-10 16:42:57 -0500

    added and passed tests for add_to_cart

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -2,7 +2,13 @@ import { useReducer } from "react";
 import {
   UPDATE_PRODUCTS,
   UPDATE_CATEGORIES,
-  UPDATE_CURRENT_CATEGORY
+  UPDATE_CURRENT_CATEGORY,
+  ADD_TO_CART,
+  ADD_MULTIPLE_TO_CART,
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  CLEAR_CART,
+  TOGGLE_CART
 } from "./actions";
 
 export const reducer = (state, action) => {
@@ -23,7 +29,14 @@ export const reducer = (state, action) => {
       return {
         ...state,
         currentCategory: action.currentCategory
-      }
+      };
+
+    case ADD_TO_CART:
+      return{
+        ...state,
+        cartOpen: true,
+        cart: [...state.cart, action.product]
+      };
 
     default:
       return state;
